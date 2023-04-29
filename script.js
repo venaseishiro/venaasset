9cb85412f628e66fba4149cd3844941b59f44ae9
venaseishiro <132131142+venaseishiro@users.noreply.github.com>
2023-04-29 15:35:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+//let teks;
+//  let day = prompt("How's Your Day??");
+//alert("Aku mau kasi paket")
+//alert("Semoga sesuai ekspetasi yaa")
+
+let nama = prompt("Input Your Name:");
+document.write(`Welcome kak ${nama}`);
